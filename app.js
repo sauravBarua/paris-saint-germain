08b47b563fb9fd28855bdaa144a143749eac3917
sauravBarua <myallmailofficial@gmail.com>
2022-08-22 15:00:28 +0200

    add nav

--- app.js
+++
@@ -1,1 +0,0 @@
-console.log("Best five")
---
+++ app2.js
@@ -0,0 +1,44 @@
+document.getElementById("neo").addEventListener("click", function () {
+  document.getElementById("name").innerHTML = "Neo";
+});
+
+document.getElementById("leo").addEventListener("click", function () {
+  document.getElementById("name").innerHTML = "Leo";
+});
+
+document.getElementById("teo").addEventListener("click", function () {
+  document.getElementById("name").innerHTML = "Teo";
+});
+document.getElementById("geo").addEventListener("click", function () {
+  document.getElementById("name").innerHTML = "Geo";
+});
+document.getElementById("meo").addEventListener("click", function () {
+  document.getElementById("name").innerHTML = "Meo";
+});
+
+// function collectin(){
+//     const liCollectin = getElementByTagName("li");
+//     for(const li of liCollectin){
+//         console.log(li.innerText)
+//     }
+// }
+
+//Button disabled 
+// const button = document.querySelector('#button');
+
+// const disableButton = () => {
+    
+//     if(button.disabled == true){
+//       style.background = 'grey';
+//     }else{
+//       style.background = 'blue';
+//     }
+
+// };
+
+// button.addEventListener('click', disableButton);
+
+// function disableButton() {
+//   document.getElementsByTagName("button").disabled = true;
+// }
+
