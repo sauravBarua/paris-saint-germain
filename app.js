e0c40ca5803ba76178e5fb6acab363e647a0eb82
sauravBarua <myallmailofficial@gmail.com>
2022-08-23 02:16:42 +0200

    add onclick playerSelected function

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-// const carts = document.querySelectorAll(".addCart");
-
-
-// for (const cart of carts) {
-//   cart.addEventListener("click", function () {
-//     const node = document.createElement("li");
-
-//     const textnode = document.createTextNode("Water");
-
-//     node.appendChild(textnode);
-
-//     document.getElementById("playerName").appendChild(node);
-//   });
-// }
-
+function playerSelected(data) {
+    const playersList = document.getElementById("playerName");
+    const li = document.createElement("li");
+    li.innerText = data;
+    playersList.appendChild(li);
+    console.log("button clicked");
+  }
